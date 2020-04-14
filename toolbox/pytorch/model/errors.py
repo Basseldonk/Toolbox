79873480c68d06951b23de7c107d99e3ed3db03a
Bram van Asseldonk <Bram@2xp.nl>
2020-04-14 09:57:28 +0200

    Added a TrainingErrorException class

--- toolbox/pytorch/model/errors.py
+++ toolbox/pytorch/model/errors.py
@@ -12,3 +12,18 @@ class TrainingInterruptedException(Exception):
 
     def __str__(self):
         return 'TrainingInterrupted, {0} '.format(self.message)
+
+class TrainingErrorException(Exception):
+    """ Exception for when training runs into an error. """
+    def __init__(self, error=None, epoch=0, batch=0):
+        self.error = error
+        self.epoch = epoch
+        self.message = f'training was interrupted due to an error at epoch {epoch}.'
+        self.batch = batch
+        if batch > 0:
+            self.message = self.message.replace('.', f', batch {batch}.')
+        if not error is None:
+            self.message = self.message + f'\n{error}'
+
+    def __str__(self):
+        return 'TrainingError, {0} '.format(self.message)
